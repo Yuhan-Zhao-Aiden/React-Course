f7e3caf0ecdc2a7ef1610947b7e5ab6a95e5031d
Yuhan Zhao <aidenzhao@hotmail.com>
2024-03-24 19:50:02 -0400

    Added useState

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,11 +1,11 @@
+import { useState } from "react";
+
 import { CORE_CONCEPTS } from "./data";
 import Header from "./components/Header/Header.jsx";
 import CoreConcepts from "./components/CoreConcept/CoreConcept.jsx";
-import TabButton from "./components/TabButton/TabButton.jsx"
-
+import TabButton from "./components/TabButton/TabButton.jsx";
 
 function App() {
-
     function clickHandler(selectedButton) {
         console.log(selectedButton);
     }
@@ -27,12 +27,19 @@ function App() {
                 <section id="examples">
                     <h2>Examples</h2>
                     <menu>
-                        <TabButton clickFunc={() => clickHandler('components')}>Components</TabButton>
-                        <TabButton clickFunc={() => clickHandler('jsx')}>JSX</TabButton>
-                        <TabButton clickFunc={() => clickHandler('props')}>Props</TabButton>
-                        <TabButton clickFunc={() => clickHandler('state')}>State</TabButton>
+                        <TabButton clickFunc={() => clickHandler("components")}>
+                            Components
+                        </TabButton>
+                        <TabButton clickFunc={() => clickHandler("jsx")}>
+                            JSX
+                        </TabButton>
+                        <TabButton clickFunc={() => clickHandler("props")}>
+                            Props
+                        </TabButton>
+                        <TabButton clickFunc={() => clickHandler("state")}>
+                            State
+                        </TabButton>
                     </menu>
-
                 </section>
                 <h2>Time to get started!</h2>
             </main>
